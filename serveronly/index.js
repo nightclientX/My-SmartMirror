07aace69d535275d3c7c1d8c06c4ec95bcc1394c
nightclientX <149981082+nightclientX@users.noreply.github.com>
2023-11-06 17:26:18 +0100

    Delete serveronly directory

--- serveronly/index.js
+++
@@ -1,8 +0,0 @@
-const app = require("../js/app");
-const Log = require("../js/logger");
-
-app.start().then((config) => {
-	const bindAddress = config.address ? config.address : "localhost";
-	const httpType = config.useHttps ? "https" : "http";
-	Log.log(`\nReady to go! Please point your browser to: ${httpType}://${bindAddress}:${config.port}`);
-});
